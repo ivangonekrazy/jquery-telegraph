6ec2c498f7038949c7c6e5b384fa7d0010fe7cba
♥ Ivan Tam ♥ <ivan@loggly.com>
2012-08-28 17:19:03 -0700

    Changed to a more module style

--- jquery-telegraph.js
+++ jquery-telegraph.js
@@ -12,40 +12,88 @@
 
 ;(function($) {
 
+    $.telegraph = {
+        settings : { },
+        defaults : { },
+
+        // our table of cables to callbacks
+        cables : { },
+
+        register: function(cable_name) {
+            var cable = cable_name && this.cables[cable_name]
+            if (!cable) {
+                callbacks = jQuery.Callbacks()
+                this.cables[cable_name] = callbacks
+            }
+            return cable
+        },
+
+        subscribe: function(cable_name, callback) {
+            var cable = this.register(cable_name)
+            cable.add(callback)
+            return cable
+        },
+
+        publish: function(cable_name, args) {
+            var cable = this.register(cable_name)
+            cable.fire(args)
+            return cable
+        },
+
+        unsubscribe: function(cable_name, callback) {
+            var cable = this.register(cable_name)
+            cable.remove(callback)
+            return cable
+        },
+
+        clear: function(cable_name) {
+            this.cables = { }
+        }
+
+        // one-shot subscribe?
+        // deferreds?
+        // expose callback options?
+
+    }
+/*
     $.telegraph = function(channel_name) {
-        var callbacks;
-        var channel = channel_name && $.telegraph.channels[ channel_name ];
+        var callbacks
+        var channel = channel_name && $.telegraph.channels[ channel_name ]
+
+        // TODO use regexen to find channels
 
         if (!channel) {
-            callbacks = jQuery.Callbacks();
+            callbacks = jQuery.Callbacks()
             channel = {
                 publish: callbacks.fire,
                 subscribe: callbacks.add,
                 unsubscribe: callbacks.remove,
-                clear: $.telegraph.clear,
-            };
+                clear: function(){
+                            return $.telegraph.clear.call(this, channel_name)
+                       },
+            }
 
             if (channel_name) {
-                $.telegraph.channels[ channel_name ] = channel;
+                $.telegraph.channels[ channel_name ] = channel
             }
         }
 
-        return channel;
+        return channel
     }
 
     $.telegraph.clear = function(channel_name) {
         var channel = channel_name && $.telegraph.channels[ channel_name ];
 
         if (channel) {
-            $.telegraph.channels[ channel_name ] = undefined;
+            $.telegraph.channels[ channel_name ] = undefined
         }
     }
 
-    $.telegraph.channels = { };
+    $.telegraph.channels = { }
 
     $.telegraph.defaults = { }
 
     $.telegraph.settings = { }
-
+*/
 
 })(jQuery);
